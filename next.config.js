694a7baa2fea7dc32f4e7b4b4c577b36e7aa88c7
abhinav <abhinavpadige06@gmail.com>
2026-09-16 13:58:57 +0530

    Fix Next.js config: remove webpack config to rely on tsconfig paths

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: [],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    }
-    return config
-  }
 }
 
 module.exports = nextConfig
